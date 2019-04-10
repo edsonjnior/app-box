5509b0f2e008cce17ba87cce3101811ee0ae0c8d
unknown <edsonjnior@gmail.com>
2019-04-09 22:34:25 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { routes } = require('./routes')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+  console.log('ok')
+})
+
+mongoose.connect(
+  'mongodb+srv://admin:admin@cluster0-t6vxp.mongodb.net/test?retryWrites=true',
+  {
+    useNewUrlParser: true
+  }
+)
+
+app.use((req, res, next) => {
+  req.io = io
+
+  return next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+app.use(routes)
+
+server.listen(3333, () => {
+  console.log('Server is running...')
+})
